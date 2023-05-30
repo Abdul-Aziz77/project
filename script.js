73dd4765434a6bbc34393107fe5529547c3a8f53
Double-A <gerzeliyev@mail.ru>
2023-05-30 09:37:06 +0300

    first commt

---
+++ script.js
@@ -0,0 +1,21 @@
+const arr = ['a', 'b', 'c']
+const arrObj = {
+    0: '1',
+    'b': '2',
+    2: '3'
+}
+arrObj['v'] = '111'
+
+console.log(arrObj['v'])
+
+const storename = 'Impuls';
+
+const storeDescription = {
+    budget: 10000,
+    employees: ['Igor', 'Andrew', 'Pavel'],
+    products: {
+        'phone': 100,
+        'pen': 20
+    },
+    open: true
+}
